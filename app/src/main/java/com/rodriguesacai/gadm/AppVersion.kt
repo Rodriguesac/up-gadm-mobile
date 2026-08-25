f3ae97304a9e4ed925a44e8b7d871c684130e959
Rodriguesac <rodrigues.acaiecia@gmail.com>
2026-08-25 03:00:17 -0400

    Atualiza nome e versão oficial

--- app/src/main/java/com/rodriguesacai/gadm/AppVersion.kt
+++ app/src/main/java/com/rodriguesacai/gadm/AppVersion.kt
@@ -1,6 +1,6 @@
 package com.rodriguesacai.gadm
 
 object AppVersion {
-    const val NAME = "UP GADM Mobile v2.1.0"
-    const val SHORT = "2.1.0"
+    const val NAME = "Gestor Geral v3.1.0"
+    const val SHORT = "3.1.0"
 }
